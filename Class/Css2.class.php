3cefe2bdb48f4c495c7c0374d2d327bfd3216e79
code <code@qyy.fr>
2010-10-21 19:02:07 +0000

    Ajout de XhtmlTransitional XhtmlTransitional * CreeMeta quelques modifs

--- Class/Css2.class.php
+++ Class/Css2.class.php
@@ -53,21 +53,25 @@ class Css2
    * @link http://www.w3.org/TR/CSS2/media.html
    */
   const MEDIA_ALL = 'all';
+
   /**
    * Destiné aux appareils braille à retour tactile.
    * @link http://www.w3.org/TR/CSS2/media.html
    */
   const MEDIA_BRAILLE = 'braille';
+
   /**
    * Destiné à l'impression braille.
    * @link http://www.w3.org/TR/CSS2/media.html
    */
   const MEDIA_EMBOSSED = 'embossed';
+
   /**
    * Destiné aux appareils portatifs (petit écran, bande passante limitée...).
    * @link http://www.w3.org/TR/CSS2/media.html
    */
   const MEDIA_HANDHELD = 'handheld';
+
   /**
    * Destiné aux médias paginée et pour les documents affichés à l'écran en mode
    * aperçu avant impression. Consulter la section sur les médias paginés pour
@@ -76,6 +80,7 @@ class Css2
    * @link http://www.w3.org/TR/CSS2/page.html
    */
   const MEDIA_PRINT = 'print';
+
   /**
    * Destiné aux présentations projetées, par exemple avec des vidéo-projecteurs.
    * Consulter la section sur les médias paginés pour informations sur leurs
@@ -84,11 +89,13 @@ class Css2
    * @link http://www.w3.org/TR/CSS2/page.html
    */
   const MEDIA_PROJECTION = 'projection';
+
   /**
    * Destiné principalement aux écrans couleur d'ordinateur.
    * @link http://www.w3.org/TR/CSS2/media.html
    */
   const MEDIA_SCREEN = 'screen';
+
   /**
    * Destiné à des synthétiseurs vocaux. Note: CSS2 avait un type de média dénommé
    * 'aural' à cette fin. Voir l'annexe sur les feuilles de style auditives pour
@@ -97,6 +104,7 @@ class Css2
    * @link http://www.w3.org/TR/CSS2/aural.html
    */
   const MEDIA_SPEECH = 'speech';
+
   /**
    * Destiné aux médias utilisant une grille de caractères fixe (tels que
    * télétypes, les terminaux, les consoles unix, ou des appareils portables aux
@@ -106,6 +114,7 @@ class Css2
    * @link http://www.w3.org/TR/CSS2/syndata.html#pixel-units
    */
   const MEDIA_TTY = 'tty';
+
   /**
    * Destiné aux appareils du type télévision (basse résolution, couleur et
    * possibilité de défillement limitée, sons disponibles).
